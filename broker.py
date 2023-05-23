4eef57d5e0a9989cdbe0c3e0fd48beedd06fb26e
Vico Fischer <vico.fischer1@gmail.com>
2023-05-23 09:10:08 +0200

    Send data readings to MQTT broker
    Script now able to send data to the master raspberry pi via MQTT.

--- broker.py
+++ broker.py
@@ -1,9 +1,20 @@
 #! /usr/bin/python
 import time
 import Adafruit_DHT
+import paho.mqtt.client as mqtt
+
+
+client = mqtt.Client()
+
+client.username_pw_set("mqtt_user", "raspberry")
+
+client.connect("10.7.154.204", 1883)
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    client.publish("greenhouse-1/floor-1/humiture-1/temperature", temperature)
+    client.publish("greenhouse-1/floor-1/humiture-1/humidity", humidity)
 
     print(f"Temp: {temperature}°C  Humidity: {humidity}%")
 
