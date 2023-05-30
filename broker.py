ddfb576303ddf074f73a5e9a1a9bd933196c1496
Vico Fischer <vico.fischer1@gmail.com>
2023-05-30 08:55:54 +0200

    Initialise DHT11 Sensor and set data pin

--- broker.py
+++ broker.py
@@ -4,18 +4,26 @@ import Adafruit_DHT
 import paho.mqtt.client as mqtt
 
 
+# DHT11 data pin
+DHT_DATA_PIN = 4
+
+# Setup DHT11 sensor
+dht11_sensor = Adafruit_DHT.DHT11
+
+# Setup MQTT Client
 client = mqtt.Client()
 
 client.username_pw_set("mqtt_user", "raspberry")
 
 client.connect("10.7.154.204", 1883)
 
+# Read sensor data and publish
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(dht11_sensor, DHT_DATA_PIN)
 
     client.publish("greenhouse-1/floor-1/humiture-1/temperature", temperature)
     client.publish("greenhouse-1/floor-1/humiture-1/humidity", humidity)
 
     print(f"Temp: {temperature}°C  Humidity: {humidity}%")
 
-    time.sleep(1)
+    time.sleep(6)
